768a5c6e375a22c99e4095ea88f7f1d4661743d1
fsam95 <f.samir@mail.utoronto.ca>
2017-01-21 15:58:56 -0500

    package.json contains dependencies that will be downloaded when 'npm install' command is run; server.js contains code that runs neural network with a given image

---
+++ FaceCreeper/server.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var PythonShell = require('python-shell');
+var multer = require('multer');
+
+var upload = multer({dest: 'upload/'});
+var type = upload.single('img');
+
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
+
+
+app.post('/process', type, function(req, res){
+  console.log(req.file);
+  var options = {
+    args: [img]
+  };
+  PythonShell.run('nn_real_data.py', options, function(err, results){
+    if (err) throw err;
+    console.log(results);
+    return res.send(JSON.stringify(results));
+  });
+});
+
+app.listen(3000);
+console.log("Listening on port 3000");
